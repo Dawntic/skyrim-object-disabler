8778798721f603b44c0e74074ae4f8e0e6ef2168
Dawntic <Dawntic@users.noreply.github.com>
2026-06-10 22:29:51 +1000

    feat: basic config

---
+++ PCH.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <detours/detours.h>
+
+#ifdef NDEBUG
+#	include <spdlog/sinks/basic_file_sink.h>
+#else
+#	include <spdlog/sinks/msvc_sink.h>
+#endif
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+namespace logger = SKSE::log;
+
+using namespace std::literals;
+
+#define DLLEXPORT __declspec(dllexport)
+
+namespace stl {
+    using namespace SKSE::stl;
+
+    template <class T>
+    void write_thunk_call(std::uintptr_t a_src) {
+        auto& trampoline = SKSE::GetTrampoline();
+        T::func = trampoline.write_call<5>(a_src, T::thunk);
+    }
+
+    template <class T>
+    void detour_thunk(REL::RelocationID a_relId) {
+        T::func = a_relId.address();
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(reinterpret_cast<PVOID*>(&T::func), reinterpret_cast<PVOID>(T::thunk));
+        DetourTransactionCommit();
+    }
+}
